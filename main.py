6e8d516673141887e880a32ceefb6e2b26c2cda7
Val <yermakvaleriy@gmail.com>
2024-04-08 12:11:05 +0300

    Finish

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],)
+    allow_headers=["*"],
+)
 
 
 app.include_router(auth.router, prefix="/api")
