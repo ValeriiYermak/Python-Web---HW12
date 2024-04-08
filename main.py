7f576f1aa9ff2b00b11f4d49137c71afab0e7aad
Val <yermakvaleriy@gmail.com>
2024-04-08 12:10:18 +0300

    29/03/2024 Finish

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ from src.routes import auth
 
 app = FastAPI()
 origins = ["*"]
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],)
 
 
 app.include_router(auth.router, prefix="/api")
@@ -33,10 +33,10 @@ async def healthchecker(db: AsyncSession = Depends(get_db)):
         result = await db.execute(text("SELECT 1"))
         result = result.fetchone()
         if result is None:
-            raise HTTPException(status_code=500,
-                                detail="Database is not configured correctly")
+            raise HTTPException(
+                status_code=500, detail="Database is not configured correctly"
+            )
         return {"message": "Welcome to FastAPI!"}
     except Exception as e:
         print(e)
-        raise HTTPException(status_code=500,
-                            detail="Error connecting to the database")
+        raise HTTPException(status_code=500, detail="Error connecting to the database")
